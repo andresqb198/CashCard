b8430fb9e06a881fee132c6b622250656b6dafdb
andresqb198 <andresqb198@gmail.com>
2023-05-16 21:24:59 -0500

    First Commit

---
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,21 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void myFirstTest() {
+		assertThat(42).isEqualTo(42);
+	}
+
+}
